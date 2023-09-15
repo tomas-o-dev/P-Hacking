b2dd855b3e45b5efa093c440df427e5c2965b785
tomas-o <todaniel649@gmail.com>
2023-09-15 14:23:52 +0800

    Add files via upload

--- P_HAKN/__init__.py
+++ P_HAKN/__init__.py
@@ -1,9 +1,13 @@
 
-from .p_hacking import run_friedman, ph_pvals, cq_mph, ap2h0
+from .p_hacking import run_friedman, ph_pvals, cq_mph, filtr_ap2h0, filtr_psig, compare_avgranks, compare_avgranks_lf, sort_dict_byval
 from .phak_cdgraf import nemenyi_cdgraph 
 
 __all__ = ['run_friedman',
            'ph_pvals',
+           'filtr_ap2h0',
+           'filtr_psig',
+           'compare_avgranks',
+           'compare_avgranks_lf',
+           'sort_dict_byval',
            'nemenyi_cdgraph',
-           'ap2h0',
            'cq_mph']
